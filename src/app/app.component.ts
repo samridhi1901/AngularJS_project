617f8207b35ad55885c21e848ea032ec8cb86aa6
samridhi singh <gurjarsam6@gmail.com>
2025-03-22 15:10:39 +0530

    UC5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,16 @@ export class AppComponent {
   title = 'HelloWorldApp';
   bridgeLabzLogo: string = 'assets/logo.jpg';
   userName: string = '';
+  errorMessage = "";
+  validateUserName() {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
+    
+    if (!this.userName.match(namePattern)) {
+      this.errorMessage = "Invalid Name! Start with a capital letter & use at least 3 letters.";
+    } else {
+      this.errorMessage = "";
+    }
+  }
 
   
   
